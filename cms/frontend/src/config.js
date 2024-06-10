c65251955b1ee13e28feee9d0e9f667e03e56881
Hammad Rao <106843117+hammadrao891@users.noreply.github.com>
2024-06-10 13:07:13 +0500

    Delete cms directory

--- cms/frontend/src/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    appId:"9ac7c974-eb76-4620-ac6a-afae4308e657",
-    redirectUri:"http:localhost:3000/dashboard",
-    scopes: [
-        'user.read'
-    ],
-    // authority :""
-}
